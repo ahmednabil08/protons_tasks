880cb68bd1be5524661f0a002870fb4038a0258d
ahmednabil08 <ahmed.n.nazieh@gmail.com>
2025-08-10 03:57:42 +0300

    update task10protons.py

--- task10protons.py
+++ task10protons.py
@@ -1,4 +1,4 @@
-# Task10 Ex.1
+# # # Task10 Ex.1
 treasure = {
     "clues": ["beach", "cave", "waterfall"],
     "locations": {
@@ -26,12 +26,39 @@ for location in treasure["locations"]:
         if i == "gold coin":
             print(f"Treasure found in {location}")
     print(f"{location} : {', '.join(items)}")
+# # ////////////////////////////////////////////////////////////////////
+# # # Task10 Ex.2
+brackets = input("Brackets: ")
+items = brackets.split()
+for item in items:
+    stack = []
+    balanced = True
+    for item in items:
+        if item == "(" or item == "{" or item == "[":
+            stack.append(item)
+        elif item == ")" or item == "}" or item == "]":
+            if not stack:
+                balanced = False
+                break
+            last = item[-1]
+        if item == ")" and last != "(" or item == "]" and last != "[" or item == "}" and last != "{":
+            balanced = False
+            break
+        if balanced and not stack:
+            print(f"{item} Balanced")
+        else:
+            print(f"{item} Imbalanced") 
 # ////////////////////////////////////////////////////////////////////
-# Task10 Ex.2
-sent = input("Sentence\n")
-li = sent.split()
-for item in li:
-    reversed_word = ""
-    for i in range (len(item) -1, -1, -1):
-        reversed_word += item[i]
-    print(reversed_word, end=" ")
+# # Task10 Ex.3
+import heapq
+customers = ["Alex", "Bob", "Sarah", "Maria", "Mike"]
+vips = ["Alex", "Maria"]
+vip_queue = []
+regular_queue = []
+for customer in customers:
+    if customer in vips:
+        vip_queue.append(customer)
+    else:
+        regular_queue.append(customer)
+serve = vip_queue + regular_queue
+print(serve)
